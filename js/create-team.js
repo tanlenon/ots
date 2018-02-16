5fb065f2463b8735bf8e3791bb6f7ee03898f29e
tanlenon <tlenon@byu.edu>
2018-02-16 13:42:33 -0600

    Added color picker mobile

--- js/create-team.js
+++ js/create-team.js
@@ -1,38 +1,81 @@
-const fileInput = document.getElementById('img-file-input');
+$( document ).ready(function() {
+    const fileInput = document.getElementById('img-file-input');
 
-fileInput.addEventListener('change', (e) =>  {
+    fileInput.addEventListener('change', (e) =>  {
 
-    var files = e.target.files;
+        var files = e.target.files;
 
-    var show = $('#show-logo')
+        var show = $('#show-logo')
 
-    // show.empty();
+        // show.empty();
 
-    // show.append('<img src="" id="image">');
+        // show.append('<img src="" id="image">');
 
-    var img = $('#loaded-logo');
+        var img = $('#loaded-logo');
 
-    img.attr("src",window.URL.createObjectURL(files[0]));
+        img.attr("src",window.URL.createObjectURL(files[0]));
 
-});
+    });
 
 
 
-const colorChange1 = document.getElementById('colorChoice1');
-const colorChange2 = document.getElementById('colorChoice2');
+    const colorChange1 = document.getElementById('colorChoice1');
+    const colorChange2 = document.getElementById('colorChoice2');
 
-colorChange1.addEventListener('change', (e) =>  {
+    colorChange1.addEventListener('change', (e) =>  {
 
-    var other = $('#show-color-1');
+        var other = $('#show-color-1');
 
-    other.css('background-color', $('#colorChoice1').val())
+        other.css('background-color', $('#colorChoice1').val())
 
-});
+    });
 
-colorChange2.addEventListener('change', (e) =>  {
+    colorChange2.addEventListener('change', (e) =>  {
 
-    var other = $('#show-color-2');
+        var other = $('#show-color-2');
 
-    other.css('background-color', $('#colorChoice2').val())
+        other.css('background-color', $('#colorChoice2').val())
+
+    });
+
+    $('#colorChoice1').ColorPicker({
+        color: '#0000ff',
+        onShow: function (colpkr) {
+            $(colpkr).fadeIn(500);
+            return false;
+        },
+        onHide: function (colpkr) {
+            $(colpkr).fadeOut(500);
+            return false;
+        },
+        onChange: function (hsb, hex, rgb) {
+            var other = $('#show-color-1');
+            var field = $('#colorChoice1');
+            // $('#colorChoice1').val('#' + hex);
+            other.css('background-color', '#' + hex);
+            field.css('background-color', '#' + hex);
+
+        }
+    });
+
+    $('#colorChoice2').ColorPicker({
+        color: '#0000ff',
+        onShow: function (colpkr) {
+            $(colpkr).fadeIn(500);
+            return false;
+        },
+        onHide: function (colpkr) {
+            $(colpkr).fadeOut(500);
+            return false;
+        },
+        onChange: function (hsb, hex, rgb) {
+            var other = $('#show-color-2');
+            var field = $('#colorChoice2');
+            // $('#colorChoice2').val('#' + hex);
+            other.css('background-color', '#' + hex);
+            field.css('background-color', '#' + hex);
+
+        }
+    });
 
 });
